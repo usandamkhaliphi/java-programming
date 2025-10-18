96748eabb02f0dfd0064f4cb38f1363c820a4358
Usanda Mkhaliphi <164313294+usandamkhaliphi@users.noreply.github.com>
2025-10-18 21:18:34 +0200

    Add NumberGuesser game implementation

---
+++ Practice Projects/NumberGuesser.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;    
+
+public class NumberGuesser 
+{
+
+   
+   public static int generateRandomNumber() 
+   {
+      return (int) (Math.random() * 1000) + 1;  
+   }
+
+   
+      public static void playGame() 
+      {
+         Scanner scanner = new Scanner(System.in); 
+         int secretNumber = generateRandomNumber();  
+         int guess = 0;
+   
+         System.out.println("Guess a number between 1 and 1000:");
+   
+         
+         while (guess != secretNumber) 
+         {
+            guess = scanner.nextInt();  
+            if (guess < secretNumber) 
+            {
+               System.out.println("Too low! Try again.");
+            } else if (guess > secretNumber) 
+            
+            {
+               System.out.println("Too high! Try again.");
+            } else 
+            
+            {
+               System.out.println("Congratulations! You guessed the number.");
+            }
+         }
+         scanner.close();     }
+   
+     
+      public static void main(String[] args) {
+         playGame(); 
+      }
+}
